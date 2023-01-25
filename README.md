2e753f2e89543a1b3c264c6219b0ee29e427d686
Des1ynY <i.smetanin.dev@gmail.com>
2023-01-25 12:23:39 +0500

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wine Cloud
+
+IoT cloud application.
---
+++ android/app/src/main/kotlin/com/github/des1yny/wine_cloud_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.des1yny.wine_cloud_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
